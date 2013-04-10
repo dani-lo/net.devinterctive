5b7f3c3b03ad7e9fcf8126352b532b9f4f11803f
dani <dani@dev.(none)>
2013-04-10 11:41:45 +0100

    - working jscompile grunt task

--- grunt.js
+++ grunt.js
@@ -1,9 +1,28 @@
 module.exports = function(grunt) {
 
+  var sass = {
+    libs  : ["lib", "module"],
+    src   : "src/sass",
+    dest  : "deploy/public/css",
+    files : "src/sass/**/*.sass"
+  },  js = {
+    src   : "src/scripts",
+    dest  : "deploy/public/js",
+    files : "src/scripts/**/*.+(coffee|mustache)"
+  };
+
+
   // Configuration goes here
   grunt.initConfig({
-
+    pkg: grunt.file.readJSON('package.json'),
+    coffee: {
+      app: {
+        src: ['src/scripts/**.coffee'],
+        dest: 'deploy/public/js/**.js'
+      }
+    },
     // Configure the copy task to move files from the development to production folders
+    // NOt used
     copy: {
       target: {
         files: {
@@ -11,13 +30,20 @@ module.exports = function(grunt) {
         }
       }
     },
+    watch: {
+      jscompile : {
+        files: 'src/scripts/*',
+        tasks: ['coffee']
+      }
+    }
   });
+
 
   // Load plugins here
   grunt.loadNpmTasks('grunt-contrib');
 
    // Define your tasks here
-  grunt.registerTask('default', ['copy']);
+  grunt.registerTask('default', ['watch']);
 
   // Define your tasks here
 
