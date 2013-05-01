ca19a33a28d80032dd550c799199df6e366a0296
dani <dani@dev.(none)>
2013-05-01 11:15:58 +0100

    - styles and markup ofnalised, no cross-browser tsting, no interaction yet (structure only)

--- deploy/app.rb
+++ deploy/app.rb
@@ -17,7 +17,7 @@ set :haml, {
 class Devinteractive < Sinatra::Application
 
     @app_paths = {
-      :routes => "/Users/dani/Sites/devinteractive.net/app/net.devinterctive/deploy/routes/"
+      :routes => "/home/dani/dev/devinteractive.net/www/deploy/routes/"
     }
 
     class << self
--- deploy/routes/pages/akqa.rb
+++ deploy/routes/pages/akqa.rb
@@ -1,3 +1,7 @@
+get '/akqa' do
+    haml :"pages/akqa"
+end
+
 get '/akqa-test' do
     haml :"pages/akqa-test"
 end
